ec9f70c84d1efa4824d51b4d8b7daee4265d3a79
Rafael Daza <radazn1998@gmail.com>
2024-09-25 10:57:52 -0500

    Aplicacion de peliculas con react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BuscadorPeliculas } from './BuscadorPeliculas'
+import './styles/buscador.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BuscadorPeliculas></BuscadorPeliculas>
+  </StrictMode>,
+)
